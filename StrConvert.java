3cecda3bd7f721b662ec822a226224f86bd385d4
Bavanidevi <bavab.tech@gmail.com>
2017-08-05 17:25:46 +0530

    Update StrConvert.java

--- StrConvert.java
+++ StrConvert.java
@@ -2,11 +2,9 @@ class StrConvert
 {
   public static void main(String []args){
     String strte = "100";
-    
-    int iTest = Integer.parseInt(strTest);
-    System.out.println("Actual String:"+ strTest);
-    System.out.println("Converted to Int:" + iTest);
-    
-    System.out.println("Arithmetic Operation on Int:" + (iTest/4));
+    int itest = Integer.parseInt(strte);
+    System.out.println("Actual String:"+ strte);
+    System.out.println("Converted to Int:" + itest);
+    System.out.println("Arithmetic Operation on Int:" + (itest/4));
   }
 }
