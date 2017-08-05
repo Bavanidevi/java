72b7f33a31f7a8ba60e7e33cd23b1591f8679d59
Bavanidevi <bavab.tech@gmail.com>
2017-08-05 11:31:57 +0530

    Update StrConvert.java

--- StrConvert.java
+++ StrConvert.java
@@ -1,14 +1,12 @@
-lass StrConvert{
+class StrConvert
+{
   public static void main(String []args){
-    String strTest = "100";
-    //This statement results in a compilation error as you
-    //cannot do arithmetic operation on Strings
-    //System.out.println("Using String:" + (strTest/4));
-    //Convert the String to Integer
+    String strte = "100";
+    
     int iTest = Integer.parseInt(strTest);
     System.out.println("Actual String:"+ strTest);
     System.out.println("Converted to Int:" + iTest);
-    //This will now show some arithmetic operation
+    
     System.out.println("Arithmetic Operation on Int:" + (iTest/4));
   }
 }
